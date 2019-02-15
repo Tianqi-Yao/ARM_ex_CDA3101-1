c4f78cf13b581057efa9acca37b3a5e8ea21a223
Nicholas Ionata <n.ionata129@gmail.com>
2019-02-15 12:51:25 -0500

    Implemented main functionality

--- main.s
+++ main.s
@@ -14,6 +14,11 @@
 	int_b_input_buffer:		.space 4
 	char_input_buffer: 		.space 1
 
+	add_char:				.asciz "+"
+	sub_char:				.asciz "-"
+	mul_char:				.asciz "*"
+	div_char:				.asciz "/"
+
 .global main
 .text
 
@@ -26,27 +31,45 @@ main:
 	bl scanf
 
 	//Load registers with their values from the saved addresses
+	ldr x0, =decimal_specifier
 	ldr x1, =int_a_input_buffer
 	ldr x1, [x1]
 	ldr x2, =int_b_input_buffer
 	ldr x2, [x2]
 	ldr x3, =char_input_buffer
 
+	//If input char is + add
+	cmp	x3, add_char
+	beq ADD
 
+	//If input char is - subtract
+	cmp	x3, sub_char
+	beq SUB
 
-	ldr x0, =decimal_specifier
-	bl DIV
+	//If input char is * multiply
+	cmp	x3, mul_char
+	beq MUL
+
+	//If input char is / divide
+	cmp	x3, div_char
+	beq DIV
 
 ADD:
 	add x1, x1, x2
 	bl printf
 
 SUB:
+	sub x1, x1, x2
+	bl printf
 
 MUL:
+	mul x1, x1, x2
+	bl printf
 
 DIV:
 	cbz x2, exit
+	//div x1, x1, x2
+	bl printf
 
 exit:
 	ldr x0, =newline
